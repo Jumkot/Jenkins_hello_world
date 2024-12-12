f8790a327cba51d75fc1345d47a282bb9b3c12a6
Jumkot Ubuntu <jumkot@mail.ru>
2024-12-13 02:03:29 +0700

    New web fix

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,20 @@
-#include "crow_all.h"
+#include <Wt/WApplication.h>
+#include <Wt/WEnvironment.h>
+#include <Wt/WText.h>
 
-int main() {
-    crow::SimpleApp app;
+class HelloWorldApp : public Wt::WApplication {
+public:
+    HelloWorldApp(const Wt::WEnvironment& env)
+        : Wt::WApplication(env) {
+        setTitle("Hello, World!"); // Установить заголовок
+        root()->addWidget(std::make_unique<Wt::WText>("Hello, World!"));
+    }
+};
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello World!";
-    });
+std::unique_ptr<Wt::WApplication> createApplication(const Wt::WEnvironment& env) {
+    return std::make_unique<HelloWorldApp>(env);
+}
 
-    app.port(5000).multithreaded().run();
-
-    return 0;
+int main(int argc, char** argv) {
+    return Wt::WRun(argc, argv, &createApplication);
 }
